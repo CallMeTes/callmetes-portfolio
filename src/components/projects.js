16585468b038ec66b8d74c83507eef4f257e405b
Tes <43484797+CallMeTes@users.noreply.github.com>
2021-05-23 17:17:24 -0400

    Updated the Projects section to include cards
    Information about Cards can be found here: https://tleunen.github.io/react-mdl/components/card/

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,11 +1,117 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, CardMenu, Button, IconButton } from 'react-mdl';
 
-function Projects() {
-  return (
-    <div>
-      <h1>Projects Page</h1>
-    </div>
-  );
+class Projects extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+    {/* React Tab */}
+    if(this.state.activeTab === 0) {
+      return (
+        <div>
+        {/* React - Project 1 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>
+            React Project #1
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Fusce ultrices ligula quis auctor placerat.
+            Etiam pulvinar tortor id lacinia sodales.
+            In hac habitasse platea dictumst.
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>CodePen</Button>
+            <Button colored>LiveDemo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+
+        {/* React - Project 2 */}
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>
+            React Project #2
+          </CardTitle>
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Fusce ultrices ligula quis auctor placerat.
+            Etiam pulvinar tortor id lacinia sodales.
+            In hac habitasse platea dictumst.
+          </CardText>
+          <CardActions border>
+            <Button colored>GitHub</Button>
+            <Button colored>CodePen</Button>
+            <Button colored>LiveDemo</Button>
+          </CardActions>
+          <CardMenu style={{color: '#fff'}}>
+            <IconButton name="share" />
+          </CardMenu>
+        </Card>
+
+          {/* React - Project 3 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover'}}>
+              React Project #3
+            </CardTitle>
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Fusce ultrices ligula quis auctor placerat.
+              Etiam pulvinar tortor id lacinia sodales.
+              In hac habitasse platea dictumst.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>CodePen</Button>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      )
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div><h1>This is MongoDB</h1></div>
+      )
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div><h1>This is Java</h1></div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId})}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>Java</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">
+                {this.toggleCategories()}
+              </div>
+            </Cell>
+          </Grid>
+        </section>
+      </div>
+    );
+  }
 }
-
 export default Projects;
